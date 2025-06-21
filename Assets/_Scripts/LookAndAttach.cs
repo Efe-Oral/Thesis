cdf200c55361a40a61c871f67e6cabd86704101c
efeoral <efeoral@gmail.com>
2025-06-21 17:52:03 +0200

    outline enables/disables when looking at a new object

--- Assets/_Scripts/LookAndAttach.cs
+++ Assets/_Scripts/LookAndAttach.cs
@@ -7,9 +7,14 @@ public class LookAndAttach : MonoBehaviour
 
     [SerializeField] private AutomaticDescriptor automaticDescriptor;
 
+    private GameObject currentlyHighlightedObject;
+    private Outline currentOutline;
+
+
     void Update()
     {
         AttachScript();
+        UpdateHighlight(automaticDescriptor.lastHitObject);
     }
 
     private void AttachScript()
@@ -25,8 +30,37 @@ public class LookAndAttach : MonoBehaviour
             return;
         }
 
-        // Attach Outline script
         Debug.Log("Adding Outline script");
         target.AddComponent<Outline>();
     }
+
+    private void UpdateHighlight(GameObject target)
+    {
+        // If we're still looking at the same object, do nothing
+        if (target == currentlyHighlightedObject)
+            return;
+
+        // Disable outline on previously highlighted object
+        if (currentOutline != null)
+        {
+            currentOutline.enabled = false;
+            currentlyHighlightedObject = null;
+            currentOutline = null;
+        }
+
+        // If looking at a new valid object
+        if (target != null)
+        {
+            Outline outline = target.GetComponent<Outline>();
+            if (outline == null)
+            {
+                outline = target.AddComponent<Outline>();
+            }
+
+            outline.enabled = true;
+            currentlyHighlightedObject = target;
+            currentOutline = outline;
+        }
+    }
+
 }
